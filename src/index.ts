c4aa7f93c4aa8e05179a2a3f6e780e339d1e0b77
JumboJet <pogybkoivan@gmail.com>
2021-09-25 18:51:01 +0300

    feat: feature 2

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,13 @@
 export function main(): void {
+  feature2()
   console.log('MAIN')
   feature1()
 }
 
 function feature1(): void {
   console.log('feature1')
+}
+
+function feature2(): void {
+  console.log('some calculations for feature2')
 }
